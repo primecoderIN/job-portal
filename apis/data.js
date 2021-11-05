fce60e235aa133618a8dd234ba6c2b5ea26ed691
= <=sanjeev2856@gmail.com>
2021-11-05 17:00:55 +0530

    Starter File Created

---
+++ apis/data.js
@@ -0,0 +1,7 @@
+const ActiveJobs = require("./ActiveJobs.json");
+const Applications = require("./Applications.json");
+
+module.exports = () => ({
+  ActiveJobs: ActiveJobs,
+  Applications: Applications,
+});
