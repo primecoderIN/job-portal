1039d49f6d6c4ccab2303790a72038b0fc3a10d2
= <=sanjeev2856@gmail.com>
2021-11-06 01:48:57 +0530

    Navigation and Hero Image Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import Hero from "../components/home-components/Hero";
 import Navbar from "../components/home-components/Navbar";
 
 export default function Home() {
-    return (
-      <div>
-        <Navbar />
-        <Hero />
-      </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
 }
