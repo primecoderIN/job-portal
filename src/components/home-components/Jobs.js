601de5e7c18d405a3d22091a0884b7f50ce92902
Sanjeev Kumar <sanjeev2856@gmail.com>
2021-11-27 12:19:20 +0530

    ES Lint Error FIxed

--- src/components/home-components/Jobs.js
+++ src/components/home-components/Jobs.js
@@ -3,15 +3,14 @@ import axios from "axios";
 import JobCard from "./JobCard";
 
 export default function Jobs({ jobs, setJobs, setApplyModal, setApplyID }) {
-  const getJobList = async () => {
-    const response = await axios.get(" http://localhost:5000/jobs");
-    setJobs(response.data.reverse());
-    console.log(response.data);
-  };
-
   useEffect(() => {
+    const getJobList = async () => {
+      const response = await axios.get(" http://localhost:5000/jobs");
+      setJobs(response.data.reverse());
+      console.log(response.data);
+    };
     getJobList();
-  }, []);
+  }, [setJobs]);
   return (
     <div>
       {jobs.map((job) => {
